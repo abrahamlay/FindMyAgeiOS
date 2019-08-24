a1c95c515c8b86315c4aedf903b3161e08c0dd59
abrahamlay <abraham_lay@yahoo.co.id>
2019-08-24 21:12:12 +0700

    Initial Commit

---
+++ FindMyAge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindMyAge
+//
+//  Created by Abraham Lay (ID) on 8/24/19.
+//  Copyright © 2019 Abraham Lay (ID). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FindMyAgeTests/FindMyAgeTests.swift
@@ -0,0 +1,34 @@
+//
+//  FindMyAgeTests.swift
+//  FindMyAgeTests
+//
+//  Created by Abraham Lay (ID) on 8/24/19.
+//  Copyright © 2019 Abraham Lay (ID). All rights reserved.
+//
+
+import XCTest
+@testable import FindMyAge
+
+class FindMyAgeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
